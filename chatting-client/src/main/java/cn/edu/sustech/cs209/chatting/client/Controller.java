1a7d98843f03e069bdee56a7ae2926286025ccc4
qwq-y <2090078073@qq.com>
2023-04-22 13:15:42 +0800

    尊滴狠无语！！上次的git骗我说保存上了结果又没有，全没了！！！这次重写不管三七二十一先存一下！！！

--- chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Controller.java
+++ chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Controller.java
@@ -9,13 +9,13 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.concurrent.atomic.AtomicReference;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -23,21 +23,21 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
-import javafx.scene.control.cell.CheckBoxListCell;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
@@ -45,14 +45,16 @@ public class Controller implements Initializable {
 
   @FXML
   ListView<Message> chatContentList;
-  ObservableList<Message> messageList = FXCollections.observableArrayList();
+//  ObservableList<Message> messageList = FXCollections.observableArrayList();
+
   private final String HOST = "localhost";
   private final int PORT = 8080;
-  String username;
   private Socket socket;
   private ObjectInputStream in;
   private ObjectOutputStream out;
-  List<ChatRecord> chatRecords = new ArrayList<>();
+
+  String username;
+  List<ChatRecord> records = new ArrayList<>();
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -66,16 +68,16 @@ public class Controller implements Initializable {
     if (input.isPresent() && !input.get().isEmpty()) {
       socket = new Socket();
       try {
-        InetAddress addre=InetAddress.getByName(HOST);
+        InetAddress addre = InetAddress.getByName(HOST);
 
-        InetSocketAddress socketAddress=new InetSocketAddress(addre,PORT);
+        InetSocketAddress socketAddress = new InetSocketAddress(addre, PORT);
         socket.connect(socketAddress);
 
         out = new ObjectOutputStream(socket.getOutputStream());
         in = new ObjectInputStream(socket.getInputStream());
-//        BufferedReader bin = new BufferedReader(in);
         System.out.println("streams created");
 
+        // login with duplicate name check
         boolean isNameDup = true;
         do {
           username = input.get();
@@ -91,7 +93,7 @@ public class Controller implements Initializable {
           System.out.println("client rsvmsg: " + rsvmsg.getData());
           if (rsvmsg.getType() == MessageType.SUCCESS) {
             isNameDup = false;
-            messageList.add(rsvmsg);
+//            messageList.add(rsvmsg);
           }
         } while (isNameDup);
 
@@ -104,63 +106,22 @@ public class Controller implements Initializable {
       Platform.exit();
     }
 
-    chatContentList.setItems(messageList);
-    chatContentList.setCellFactory(new MessageCellFactory());
+//    chatContentList.setItems(messageList);
+//    chatContentList.setCellFactory(new MessageCellFactory());
   }
 
   @FXML
   public void createPrivateChat() {
-    AtomicReference<List<String>> selectedUsers = new AtomicReference<>(new ArrayList<>());
 
-    Stage stage = new Stage();
-    ListView<String> userSel = new ListView<>();
-//    ComboBox<String> userSel = new ComboBox<>();
-
-    List<String> userList = getUserList();
-    userSel.getItems().addAll(userList);
-
-    Button okBtn = new Button("OK");
-    okBtn.setOnAction(e -> {
-      selectedUsers.set(new ArrayList<>(userSel.getSelectionModel().getSelectedItems()));
-      stage.close();
-    });
-
-    HBox box = new HBox(10);
-    box.setAlignment(Pos.CENTER);
-    box.setPadding(new Insets(20, 20, 20, 20));
-    box.getChildren().addAll(userSel, okBtn);
-
-    // Set up the ListView with multiple selection enabled
-    userSel.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-    userSel.setPrefHeight(100);
-
-    stage.setScene(new Scene(box));
-    stage.showAndWait();
-
-    List<String> users = selectedUsers.get();
-    if (!users.isEmpty()) {
-      String chatName = String.join(", ", users);
-      ChatRecord record = getRecordByName(chatName);
-      if (record == null) {
-        ChatRecord newRecord = new ChatRecord(chatName);
-        openChat(newRecord, stage);
-      } else {
-        openChat(record, stage);
-      }
-    }
-  }
-
-  private List<String> getUserList() {
-    List<String> userList = null;
+    // get user list
+    List<String> userList = new ArrayList<>();
     try {
-      Message sndmsg = new Message(System.currentTimeMillis(), username,
-          new String[]{"default"},
-          "userList", MessageType.REQUEST);
+      Message sndmsg = new Message(System.currentTimeMillis(), username, new String[]{"default"}, "userList", MessageType.REQUEST);
       out.writeObject(sndmsg);
       out.flush();
       System.out.println("client sndmsg: " + sndmsg.getData());
 
-      Message rsvmsg = (Message)in.readObject();
+      Message rsvmsg = (Message) in.readObject();
       System.out.println("client rsvmsg: " + rsvmsg.getData());
       String userListStr = rsvmsg.getData();
       userList = new ArrayList<>(Arrays.asList(userListStr.split(",")));
@@ -169,23 +130,126 @@ public class Controller implements Initializable {
     } catch (IOException | ClassNotFoundException e) {
       e.printStackTrace();
     }
-    return userList;
-  }
 
-  private ChatRecord getRecordByName(String name) {
-    for (ChatRecord record : chatRecords) {
-      if (record.getUser().equals((name))) {
-        return record;
+    // show users
+    ObservableList<ChatRecord> chatRecords = FXCollections.observableArrayList(records);
+    ListView<ChatRecord> listView = new ListView<>(chatRecords);
+
+    listView.setCellFactory(new Callback<ListView<ChatRecord>, ListCell<ChatRecord>>() {
+      @Override
+      public ListCell<ChatRecord> call(ListView<ChatRecord> listView) {
+        return new ListCell<ChatRecord>() {
+          @Override
+          protected void updateItem(ChatRecord record, boolean empty) {
+            super.updateItem(record, empty);
+            if (record != null && !empty) {
+              setText(record.getNames().toString() + ": " + record.getMessages().get(record.getMessages().size() - 1));
+            } else {
+              setText(null);
+            }
+          }
+        };
       }
+    });
+
+    listView.setOnMouseClicked(event -> {
+      if (!listView.getSelectionModel().isEmpty()) {
+        ChatRecord record = listView.getSelectionModel().getSelectedItem();
+        // Do something with the selected record
+      }
+    });
+
+
+
+    // create checkboxes for each user
+    List<CheckBox> userCheckboxes = new ArrayList<>();
+    for (String user : userList) {
+      CheckBox checkBox = new CheckBox(user);
+      userCheckboxes.add(checkBox);
     }
-    return null;
+
+    Button okBtn = new Button("OK");
+    okBtn.setOnAction(e -> {
+      // get selected users
+      List<String> selectedUsers = new ArrayList<>();
+      for (CheckBox checkBox : userCheckboxes) {
+        if (checkBox.isSelected()) {
+          selectedUsers.add(checkBox.getText());
+        }
+      }
+      // check if chat already exists for selected users
+      boolean chatExists = false;
+      for (ChatRecord record : records) {
+        if (record.getNames().containsAll(selectedUsers)) {
+          // chat already exists, select the existing chat item
+          chatList.getSelectionModel().select(record);
+          chatExists = true;
+          break;
+        }
+      }
+      if (!chatExists) {
+        // create new chat item and select it
+        ChatRecord record = new ChatRecord(selectedUsers);
+        records.add(record);
+        chatList.getSelectionModel().select(record);
+      }
+    });
+
+    VBox box = new VBox(10);
+    box.setAlignment(Pos.CENTER);
+    box.setPadding(new Insets(20, 20, 20, 20));
+    box.getChildren().addAll(userCheckboxes);
+    box.getChildren().add(okBtn);
+    Stage stage = new Stage();
+    stage.setScene(new Scene(box));
+    stage.showAndWait();
   }
 
-  private void openChat(ChatRecord record, Stage stage) {
+
+  @FXML
+//  public void createPrivateChat() {
+//    AtomicReference<String> user = new AtomicReference<>();
+//
 //    Stage stage = new Stage();
-    ChatPanel panel = new ChatPanel(record);
-
-  }
+//    ComboBox<String> userSel = new ComboBox<>();
+//
+//    try {
+//      Message sndmsg = new Message(System.currentTimeMillis(), username,
+//          new String[]{"default"},
+//          "userList", MessageType.REQUEST);
+//      out.writeObject(sndmsg);
+//      out.flush();
+//      System.out.println("client sndmsg: " + sndmsg.getData());
+//
+//      Message rsvmsg = (Message) in.readObject();
+//      System.out.println("client rsvmsg: " + rsvmsg.getData());
+//      String userListStr = rsvmsg.getData();
+//      List<String> userList = new ArrayList<>(Arrays.asList(userListStr.split(",")));
+////      userList.remove(username);
+//
+//      userSel.getItems().addAll(userList);
+//
+//    } catch (IOException | ClassNotFoundException e) {
+//      e.printStackTrace();
+//    }
+//
+//    Button okBtn = new Button("OK");
+//    okBtn.setOnAction(e -> {
+//      user.set(userSel.getSelectionModel().getSelectedItem());
+//      stage.close();
+//    });
+//
+//    HBox box = new HBox(10);
+//    box.setAlignment(Pos.CENTER);
+//    box.setPadding(new Insets(20, 20, 20, 20));
+//    box.getChildren().addAll(userSel, okBtn);
+//    stage.setScene(new Scene(box));
+//    stage.showAndWait();
+//
+//    // TODO: if the current user already chatted with the selected user, just open the chat with that user
+//    // TODO: otherwise, create a new chat item in the left panel, the title should be the selected user's name
+//
+//  }
 
   /**
    * A new dialog should contain a multi-select list, showing all user's name. You can select
@@ -198,8 +262,60 @@ public class Controller implements Initializable {
    */
   @FXML
   public void createGroupChat() {
-
   }
+
+
+  public void openChat(ChatRecord record) {
+    // 创建 UI 元素
+    BorderPane root = new BorderPane();
+    TextArea chatArea = new TextArea();
+    TextField inputField = new TextField();
+    Button sendButton = new Button("Send");
+
+    // 设置 UI 元素属性
+    chatArea.setEditable(false);
+    chatArea.setWrapText(true);
+    inputField.setPromptText("Type your message here...");
+
+    // 添加 UI 元素到容器中
+    root.setCenter(chatArea);
+    HBox inputBox = new HBox(10, inputField, sendButton);
+    inputBox.setPadding(new Insets(10));
+    root.setBottom(inputBox);
+
+    // 创建一个 Scene 并设置到 Stage 中
+    Scene scene = new Scene(root, 400, 400);
+    Stage stage = new Stage();
+    stage.setTitle(record.getNames());
+    stage.setScene(scene);
+
+    // 在 sendButton 被点击时发送消息
+    sendButton.setOnAction(event -> {
+      String message = inputField.getText().trim();
+      if (!message.isEmpty()) {
+        String formattedMessage = String.format("[%s] %s\n", LocalDateTime.now(), message);
+        chatArea.appendText(formattedMessage);
+        inputField.clear();
+      }
+    });
+
+    // 在窗口关闭时通知聊天记录保存
+//      stage.setOnCloseRequest(event -> {
+//        // 保存聊天记录
+//        record.saveChatRecord(chatArea.getText());
+//      });
+
+    // 加载保存的聊天记录
+    List<String> messages = record.getMessages();
+    if (messages != null && !messages.isEmpty()) {
+      String messagesStr = String.join("\n", messages);
+      chatArea.setText(messagesStr);
+    }
+
+    // 显示窗口
+    stage.show();
+  }
+
 
   /**
    * Sends the message to the <b>currently selected</b> chat.
@@ -220,55 +336,28 @@ public class Controller implements Initializable {
     }
   }
 
-  class ChatRecord {
-    private String user;    // the opposite user
-    private List<String> messages = new ArrayList<>();
+  private class ChatRecord {
 
-    public ChatRecord(String user) {
-      this.user = user;
+    List<String> names = new ArrayList<>();   // the opposite user
+    List<String> messages = new ArrayList<>();
+
+    public ChatRecord(List<String> names) {
+      this.names = names;
     }
 
-    public String getUser() {
-      return user;
+    public List<String> getNames() {
+      return names;
     }
 
     public List<String> getMessages() {
       return messages;
     }
-
-    public boolean deletePieceOfMessage(String m) {
-      return messages.remove(m);
-    }
-  }
-
-  class ChatPanel extends BorderPane {
-    private Label titleLabel = new Label();
-    private ListView<String> messageListView = new ListView<>();
-
-    public ChatPanel(ChatRecord record) {
-      setTitle(record.getUser());
-      setMessages(record.getMessages());
-      this.setTop(titleLabel);
-      this.setCenter(messageListView);
-    }
-
-    public void setTitle(String title) {
-      titleLabel.setText(title);
-    }
-
-    public void setMessages(List<String> messages) {
-      messageListView.getItems().addAll(messages);
-    }
-
-    public void addMessage(String message) {
-      messageListView.getItems().add(message);
-    }
   }
 
   /**
-   * You may change the cell factory if you changed the design of {@code Message} model.
-   * Hint: you may also define a cell factory for the chats displayed in the left panel,
-   * or simply override the toString method.
+   * You may change the cell factory if you changed the design of {@code Message} model. Hint: you
+   * may also define a cell factory for the chats displayed in the left panel, or simply override
+   * the toString method.
    */
   private class MessageCellFactory implements Callback<ListView<Message>, ListCell<Message>> {
 
